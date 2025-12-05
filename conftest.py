5b5c39b5ba16b105db5d5ea58c2215fded7e0264
Maryllinn <165575036+Maryllinn@users.noreply.github.com>
2025-12-05 19:30:09 +0300

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture
+def browser():
+    # Настройка опций Chrome
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
+    
+    # Инициализация драйвера
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=chrome_options
+    )
+    
+    # Неявное ожидание
+    driver.implicitly_wait(10)
+    
+    yield driver
+    
+    # Закрытие браузера после теста
+    driver.quit()
+
+@pytest.fixture
+def base_url():
+    return "https://ya.ru"
